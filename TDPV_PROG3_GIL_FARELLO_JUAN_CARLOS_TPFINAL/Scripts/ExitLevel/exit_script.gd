21973dc923a44841b7909d6bc8653a32a17f6d28
Juan Speed <jsdev4@users.noreply.github.com>
2020-01-12 16:40:53 -0300

    Added tilemaps as parallax background
    Added tilemaps with buildings (near and far).
    Saved as scenes to modify later and can add a shader to get a more deep sensation.
    Sligthy optimiztion in trees scenes.

--- TDPV_PROG3_GIL_FARELLO_JUAN_CARLOS_TPFINAL/Scripts/ExitLevel/exit_script.gd
+++ TDPV_PROG3_GIL_FARELLO_JUAN_CARLOS_TPFINAL/Scripts/ExitLevel/exit_script.gd
@@ -1,11 +1,8 @@
 extends Node
 signal has_finished
+
 func _ready():
 	connect("has_finished",get_node("../Player"),"_has_finished_the_level")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_ExitLevel_body_entered(body):
